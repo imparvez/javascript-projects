c90d8690c4bbf4ec5eb67cc80edb5e9d5d7975e4
Parvez.Shaikh <parvez.shaikh@bookmyshow.com>
2019-05-12 13:43:28 +0530

    Hooks updated for ReactJS

--- hooks-in-react/src/App.js
+++ hooks-in-react/src/App.js
@@ -1,96 +1,76 @@
-import React, { useState } from 'react'; // useState is a hook
-import './app.css';
+import React, { useState } from 'react'
+import './app.css'
 
-function Todo({ todo, index, completeTodo, deleteTodo}) {
-  return <div style={{textDecoration: todo.isCompleted ? 'line-through' : '' }} className="todo">
-    {todo.text}
-    <div>
-      <button onClick={() => completeTodo(index)}>Complete</button>
-      <button onClick={() => deleteTodo(index)}>x</button>
+function Todo({ index, todo, completeTodo, deleteTodo }) {
+  return (
+    <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className="todo">
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Completed</button>
+        <button onClick={() => deleteTodo(index)}>x</button>
+      </div>
     </div>
-  </div>;
+  )
 }
 
 function TodoForm({ addTodo }) {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState('')
 
   const handleSubmit = e => {
-    e.preventDefault();
-
-    if(!value) return;
-    addTodo(value);
-    setValue('');
+    e.preventDefault()
+    if(!value) return
+    addTodo(value)
+    setValue('')
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-        placeholder="Add Todo..."
-      />
+      <input type="text" value={value} onChange={e => setValue(e.target.value)} placeholder="Add Todo"/>
     </form>
   )
 }
 
-function Counter() {
-  const [count, setCount] = useState(0); // Calling useState aka hook in a functional component to set local state to it.
-
-  return (
-    <div>
-      <p>You click count {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        click
-      </button>
-    </div>
-  )
-}
-
-function App(){
+function App() {
   const [todos, setTodos] = useState([
     {
-      text: 'Learn React',
+      text: 'Todo 1',
       isCompleted: false
     },{
-      text: 'Meet friend on Lunch',
+      text: 'Todo 2',
       isCompleted: false
     },{
-      text: 'Build really cool app',
+      text: 'Todo 3',
       isCompleted: false
-    },
+    }
   ])
 
   const addTodo = text => {
-    const newTodos = [...todos, { text, isCompleted: false }];
-
-    setTodos(newTodos);
+    const newTodos = [...todos, { text, isCompleted: false }]
+    setTodos(newTodos)
   }
 
   const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = true;
-    setTodos(newTodos);
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true
+    setTodos(newTodos)
   }
 
   const deleteTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
   }
 
   return (
-    <div className="app">
+    <div className="App">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo} />
+            <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo} />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
-      <Counter />
     </div>
   )
 }
 
-export default App;
+export default App
