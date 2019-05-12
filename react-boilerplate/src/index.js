3282e8f8d0eadfa60d4334ae4f883a5c654294a6
Parvez.Shaikh <parvez.shaikh@bookmyshow.com>
2019-05-12 13:41:49 +0530

    React BoilerPlate updated

---
+++ react-boilerplate/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.scss'
+
+function Index() {
+    return <div>Welcome to React Boilerplate!</div>
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
